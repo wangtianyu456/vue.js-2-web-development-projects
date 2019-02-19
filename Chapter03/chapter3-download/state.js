3f45bd752c55128bec6939dcd9fa0ace68c4fb55
wangtianyu <769423688@qq.com>
2019-02-19 18:11:36 +0800

    chapter 堡垒游戏 updated

--- Chapter03/chapter3-download/state.js
+++ Chapter03/chapter3-download/state.js
@@ -7,7 +7,19 @@ var currentPlayingCard = null
 
 // The consolidated state of our app
 var state = {
-  // World
+  // World 世界
   worldRatio: getWorldRatio(),
-  // TODO Other things
+  // Game 游戏
+  turn: 1, // 第几轮
+  players: [ // 玩家
+    {
+      name: 'Wang Tianyu'
+    },
+    {
+      name: 'William'
+    }
+  ],
+  currentPlayerIndex: Math.round(Math.random()), // 随机使用0或1来决定谁先行动
+  testHand: [],
+  activeOverlay: null // 用户界面，当activeOverlay没有定义时，显示hand组件
 }
