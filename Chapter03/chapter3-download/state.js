d821164ff4f0630d77514058dc76e7b2ffd5ff04
wangtianyu <769423688@qq.com>
2019-02-26 00:28:58 +0800

    完成chapter03 城堡游戏

--- Chapter03/chapter3-download/state.js
+++ Chapter03/chapter3-download/state.js
@@ -7,19 +7,55 @@ var currentPlayingCard = null
 
 // The consolidated state of our app
 var state = {
-  // World 世界
-  worldRatio: getWorldRatio(),
-  // Game 游戏
-  turn: 1, // 第几轮
-  players: [ // 玩家
-    {
-      name: 'Wang Tianyu'
+    // World 世界
+    worldRatio: getWorldRatio(),
+    // Game 游戏
+    turn: 1, // 第几轮
+    players: [ // 玩家
+        {
+            name: 'Wang Tianyu',
+            // 游戏开始时的状态
+            food: 10,
+            health: 10,
+            // 是否跳过下个回合
+            skipTurn: false,
+            // 跳过了上个回合
+            skippedTurn: false,
+            // 手牌
+            hand: [],
+            lastPlayedCardId: null,
+            dead: false,
+        },
+        {
+            name: 'William',
+            // 游戏开始时的状态
+            food: 10,
+            health: 10,
+            // 是否跳过下个回合
+            skipTurn: false,
+            // 跳过了上个回合
+            skippedTurn: false,
+            // 手牌
+            hand: [],
+            lastPlayedCardId: null,
+            dead: false,
+        }
+    ],
+    currentPlayerIndex: Math.round(Math.random()), // 随机使用0或1来决定谁先行动
+    activeOverlay: null, // 用于保存当前显示浮层的名称，如果没有浮层则为null，用户界面，当activeOverlay没有定义时，显示hand组件
+    drawPile: pile, // 玩家可以抽牌的牌堆。
+    discardPile: {}, // 弃牌堆(墓场)
+    canPlay:true, // 是否能操作，防止作弊,防止玩家在回合中重复出牌
+    get currentPlayer() {
+        return state.players[state.currentPlayerIndex]
     },
-    {
-      name: 'William'
+    get currentOpponentId() {
+        return state.currentPlayerIndex === 0 ? 1 : 0;
+    },
+    get currentOpponent() {
+        return state.players[state.currentOpponentId]
+    },
+    get currentHand() {
+        return state.currentPlayer.hand
     }
-  ],
-  currentPlayerIndex: Math.round(Math.random()), // 随机使用0或1来决定谁先行动
-  testHand: [],
-  activeOverlay: null // 用户界面，当activeOverlay没有定义时，显示hand组件
 }
--- Chapter03/chapter3-download/utils.js
+++ Chapter03/chapter3-download/utils.js
@@ -1,26 +1,26 @@
 // WORLD
 
-function getWorldRatio () {
+function getWorldRatio() {
   return 1 / 1920 * window.innerWidth
 }
 
 // GAME
 
 // Pile
-
-function drawCard () {
+// 抽牌
+function drawCard() {
   if (getDrawPileCount() === 0) {
     refillPile()
   }
 
-  const choice = Math.round(Math.random() * (getDrawPileCount() - 1)) + 1
+  const choice = Math.round(Math.random() * (getDrawPileCount() - 1)) + 1 // 1-目前牌堆中的最后一张
 
-  let	accumulation = 0
+  let accumulation = 0
   for (let k in state.drawPile) {
     accumulation += state.drawPile[k]
     if (choice <= accumulation) {
       // Draw the card from the pile
-      state.drawPile[k] --
+      state.drawPile[k]--
       return {
         id: k,
         uid: cardUid++,
@@ -30,27 +30,31 @@ function drawCard () {
   }
 }
 
-function drawInitialHand (player) {
+// 绘制初始手牌
+function drawInitialHand(player) {
   for (let i = 0; i < handSize; i++) {
     player.hand.push(drawCard())
   }
 }
 
-function addCardToPile (pile, cardId) {
+// 将当前的卡牌放到牌堆(或弃牌堆)中
+function addCardToPile(pile, cardId) {
+  // 判断当前牌堆中是否有该张牌，如果有直接++，否则将该张牌的数量初始化为1
   if (typeof pile[cardId] === 'number') {
-    pile[cardId] ++
+    pile[cardId]++
   } else {
     pile[cardId] = 1
   }
 }
 
-function refillPile () {
+// 当牌堆中的牌为空时，将墓场中的牌放到牌堆中，清空墓场
+function refillPile() {
   Object.assign(state.drawPile, state.discardPile)
   state.discardPile = {}
 }
 
-
-function getDrawPileCount () {
+// 牌堆中的牌总数
+function getDrawPileCount() {
   let result = 0
   for (let k in state.drawPile) {
     result += state.drawPile[k]
@@ -59,21 +63,21 @@ function getDrawPileCount () {
 }
 
 // Card play
-
-function applyCardEffect (card) {
+// 应用卡牌效果
+function applyCardEffect(card) {
   state.currentPlayer.lastPlayedCardId = card.id
   card.def.play(state.currentPlayer, state.currentOpponent)
   // Check if the stats (health, food) are not outside the boundaries
   state.players.forEach(checkStatsBounds)
 }
 
-function getLastPlayedCard (player) {
+function getLastPlayedCard(player) {
   return cards[player.lastPlayedCardId]
 }
 
 // Player
 
-function checkStatsBounds (player) {
+function checkStatsBounds(player) {
   // Health
   if (player.health < 0) {
     player.health = 0
@@ -89,10 +93,10 @@ function checkStatsBounds (player) {
   }
 }
 
-function checkPlayerLost (player) {
+function checkPlayerLost(player) {
   player.dead = (player.health === 0 || player.food === 0)
 }
 
-function isOnePlayerDead () {
+function isOnePlayerDead() {
   return state.players.some(p => p.dead)
 }
